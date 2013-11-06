359edbdb0d1a231c7e85d9aabc84000d4325fbe3
Jasper Ke <jasperke@kinghood.com.tw>
2013-11-06 17:13:07 +0800

    測試產生source maps

--- _build/Gruntfile.js
+++ _build/Gruntfile.js
@@ -18,34 +18,42 @@ module.exports = function(grunt) {
 				 ''
 		],
 
-		concat: {
-			dist: {
-				src: ['./src/main.js','./src/quizPool.js'],
-				dest: './tmp/main.js'
-			},
-			dev: {
-				src: ['./src/main.js','./src/quizPool.js'],
-				dest: '<%= JS_PATH %>'+'main.min.js'
-			}
-		},
-		clean: {
-			dist: ['./tmp']
-		},
+		// concat: {
+		// 	dist: {
+		// 		src: ['./src/main.js','./src/quizPool.js'],
+		// 		dest: './tmp/main.js'
+		// 	},
+		// 	dev: {
+		// 		src: ['./src/main.js','./src/quizPool.js'],
+		// 		dest: '<%= JS_PATH %>main.min.js'
+		// 	}
+		// },
+		// clean: {
+		// 	dist: ['./tmp']
+		// },
 		uglify: {
 			options: {
-				banner: '<%= banner.join("\\n") %>'
+				banner: '<%= banner.join("\\n") %>',
+				beautify : {ascii_only: true},
+				mangle: false,
+				sourceMap: '<%= JS_PATH %>main.min.map',
+				sourceMapRoot: '<%= JS_PATH %>',
+				sourceMapPrefix: 2,
+				sourceMappingURL: 'main.min.map'
 			},
 			dist: {
-				src: ['./tmp/main.js'],
-				dest: '<%= JS_PATH %>'+'main.min.js'
+				files: {
+					'<%= JS_PATH %>main.min.js': ['<%= JS_PATH %>src/quizPool.js','<%= JS_PATH %>src/main.js']
+				}
 			}
 		},
 		copy: {
 			dist: {
 				files: [
-					{expand:true, cwd:'<%= LIB_PATH %>'+'underscore', src:'underscore-min.js', dest:'<%= JS_PATH %>'},
-					{expand:true, cwd:'<%= LIB_PATH %>'+'jquery', src:'jquery.min.js', dest:'<%= JS_PATH %>'},
-					{expand:true, cwd:'<%= LIB_PATH %>'+'bootstrap/dist', src:'**', dest:'<%= BASE_PATH %>'}
+					{expand:true, cwd:'<%= LIB_PATH %>underscore', src:'underscore-min.js', dest:'<%= JS_PATH %>'},
+					{expand:true, cwd:'<%= LIB_PATH %>jquery', src:'jquery.min.js', dest:'<%= JS_PATH %>'},
+					{expand:true, cwd:'<%= LIB_PATH %>bootstrap/dist', src:'**', dest:'<%= BASE_PATH %>'},
+					{expand:true, cwd:'./src', src:'*.js', dest:'<%= JS_PATH %>src/'}
 				]
 			}
 		},
@@ -56,19 +64,20 @@ module.exports = function(grunt) {
 				options: {
 					spawn: true,
 				}
-		  	}
+			}
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
+	//grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-clean');
+	//grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('lib', ['copy']);
 
-	grunt.registerTask('product', ['concat:dist','uglify','clean']);
+	//grunt.registerTask('product', ['concat:dist','uglify','clean']);
+	grunt.registerTask('product', ['lib','uglify']);
 	grunt.registerTask('development', ['concat:dev']);
 
 	grunt.registerTask('default', ['lib','product']);
