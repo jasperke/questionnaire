d2bd748bc3675fbefb726d085ef11e6f66a1eccb
Jasper Ke <jasperke@kinghood.com.tw>
2014-01-10 12:08:10 +0800

    待診患者須依醫生過濾(只能看自已的)

--- rpc/getList.php
+++ rpc/getList.php
@@ -29,11 +29,11 @@ $SiteLoginPWD=kwut2_readini(CFG_FN,"KOALA","SitePWD");
 $db=kwcr2_mapdb('CyberSite',$SiteLoginUID,$SiteLoginPWD);
 if($db!=0){
 	if(strcmp($scopeMode,'0')==0){ // 今日全部病患
-		$s="select q.CreateTime,q.RandNum,q.Questionnaire,q.No,q.Score,u.Name,q.Weight,u.Gender,u.Birthday from MUST_Questionnaire q left outer join MUST_QuestionnaireUser u on q.No=u.No where q.OwnerID=? and DATEPART(q.CreateTime)=CURDATE() order by q.CreateTime asc";
+		$s="select q.CreateTime,q.RandNum,q.Questionnaire,q.No,q.Score,u.Name,q.Weight,u.Gender,u.Birthday from MUST_Questionnaire q left outer join MUST_QuestionnaireUser u on q.No=u.No where q.OwnerID=? and q.StaffID=? and DATEPART(q.CreateTime)=CURDATE() order by q.CreateTime asc";
 	}else{ // 今日待診病患
-		$s="select q.CreateTime,q.RandNum,q.Questionnaire,q.No,q.Score,u.Name,q.Weight,u.Gender,u.Birthday from MUST_Questionnaire q left outer join MUST_QuestionnaireUser u on q.No=u.No where q.OwnerID=? and DATEPART(q.CreateTime)=CURDATE() and Dose is null order by q.CreateTime asc";
+		$s="select q.CreateTime,q.RandNum,q.Questionnaire,q.No,q.Score,u.Name,q.Weight,u.Gender,u.Birthday from MUST_Questionnaire q left outer join MUST_QuestionnaireUser u on q.No=u.No where q.OwnerID=? and q.StaffID=? and DATEPART(q.CreateTime)=CURDATE() and Dose is null order by q.CreateTime asc";
 	}
-	$rs=read_multi_record($db, $s, array($group_id));
+	$rs=read_multi_record($db, $s, array($group_id, $_SESSION['staffId']));
 	if($rs===false){
 		$out[0]=array(900,"資料讀取失敗！(".kwcr2_geterrormsg($db,1).")");
 	}else if(isset($rs)){
